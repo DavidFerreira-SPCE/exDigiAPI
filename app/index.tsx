7d71d951230ccdad8dac7c96ea9603929aa6e08c
David <davidleandro670@gmail.com>
2025-11-04 11:03:38 -0300

    INICIANDO O INFERNO DESSA DESESTRUTURAÇÃO

---
+++ app/index.tsx
@@ -0,0 +1,35 @@
+import axios from 'axios';
+import { Image } from 'expo-image';
+import { useEffect, useState } from "react";
+import { Text, View } from "react-native";
+import styles from './styles';
+
+export default function RandomMon() {
+  const [imageUrl, setImageUrl] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const search = await axios.get('https://digi-api.com/api/v1/digimon/');
+        setImageUrl(search.data.message);
+        setError(null)
+      } catch (err) {
+        setError(null)
+      }
+    };
+    fetchData();
+  }, [error]);
+
+return (
+  <View style={styles.container}>
+        <View>
+    <Text>Seja Bem-vindo ao buscador de Digimons</Text>
+      </View>
+    <Image
+      source={require("../assets/images/logo.png")}
+      style={styles.logo}
+    />
+  </View>
+);
+}
---
+++ app/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: 'purple',
+        justifyContent: "center",
+        alignItems: "center",
+    },
+
+    logo:{
+        marginTop: 100,
+        marginRight: 200,
+        alignItems:'center',
+        width: 115,
+        height: 35,
+        position: 'absolute',
+    }
+});
+
+export default styles;
